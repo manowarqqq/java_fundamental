e5fbfb6ef4f9fee2214ca82c0bcef9c50913d18f
Victor Bystrov <bystrov1988@gmail.com>
2020-01-04 15:03:38 +0300

    refactor TaskOne, add gitignore

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
 
 public class TaskOne {
-   //Task1
-  // Приветствовать любого пользователя при вводе его имени через командную строку.
+    //Task1
+    // Приветствовать любого пользователя при вводе его имени через командную строку.
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String name = "";
-
-        while (!name.equals("close")) {
-            System.out.println("Введите имя или close для выхода");
-            name = scan.nextLine();
-            if (!name.equals("close")) {
-                System.out.println("Hello " + name);
-            } else
-                System.out.println("Bye");
+        System.out.println("Введите имя или close для выхода");
+        while (!(name = scan.nextLine()).equals("close")) {
+            System.out.println("Hello " + name);
         }
         scan.close();
     }
