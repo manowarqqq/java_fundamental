b08c1e36e88b65efbc484c771c5a79043ef7f9a8
Victor Bystrov <bystrov1988@gmail.com>
2020-01-07 19:25:31 +0300

    refactor OptionalTasks

--- src/OptionalTaskOne.java
+++ src/OptionalTaskOne.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class OptionalTaskOne {
-
-    public static void main(String[] args) {
+    // 1. Найти самое короткое и самое длинное число. Вывести найденные числа и их длину. (Самое маленькое число является самим коротким)
+    public static void findShorrtestNumber(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите размер массива");
         int arraySize = scan.nextInt();
@@ -13,8 +13,6 @@ public class OptionalTaskOne {
             array[i] = scan.nextInt();
         }
         System.out.println(Arrays.toString(array));
-
-        // 1. Найти самое короткое и самое длинное число. Вывести найденные числа и их длину. (Самое маленькое число является самим коротким)
         int min = array[0];
         for (int i = 0; i < array.length; i++) {
             if (min > array[i]) {
@@ -23,14 +21,52 @@ public class OptionalTaskOne {
         }
         System.out.println(min);
 
-        int max = array[0];
+        //  for max:
+//        int max = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (max <
+//                    array[i]) {
+//                max = array[i];
+//            }
+//        }
+//        System.out.println(max);
+    }
+
+    public static void  findNumberWithMinDifferentDigit() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите размер массива");
+        int[] array = new int[scan.nextInt()];
+        System.out.println("Введите числа миссива");
         for (int i = 0; i < array.length; i++) {
-            if (max <
-                    array[i]) {
-                max = array[i];
+            array[i] = scan.nextInt();
+        }
+        int[] numbers = Arrays.copyOf(array, array.length);
+        int minIndexinArray = 0;
+        int[] digitsCount = new int[10];
+        int[] differentDigitsCount = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            while (numbers[i] != 0) {
+                digitsCount[Math.abs((numbers[i] % 10))]++;
+                numbers[i] /= 10;
+            }
+            for (int j = 0; j < 10; j++) {
+                if (digitsCount[j] > 0) differentDigitsCount[i]++;
+            }
+            digitsCount = new int[10];
+        }
+        int min = differentDigitsCount[0];
+        for (int i = 0; i < differentDigitsCount.length; i++) {
+            if (differentDigitsCount[i] < min) {
+                min = differentDigitsCount[i];
+                minIndexinArray = i;
             }
         }
-        System.out.println(max);
+        System.out.println("Число, в котором количество различных цифр минимально: " + array[minIndexinArray]);
+    }
+
+    public static void main(String[] args) {
+findShorrtestNumber();
+findNumberWithMinDifferentDigit();
 }
 }
 
--- src/OptionalTaskTwo.java
+++ src/OptionalTaskTwo.java
@@ -3,34 +3,6 @@ import java.util.Scanner;
 
 public class OptionalTaskTwo {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите размер массива");
-        int[] array = new int[scan.nextInt()];
-        System.out.println("Введите числа миссива");
-        for (int i = 0; i < array.length; i++) {
-            array[i] = scan.nextInt();
-        }
-        int[] numbers = Arrays.copyOf(array, array.length);
-        int minIndexinArray = 0;
-        int[] digitsCount = new int[10];
-        int[] differentDigitsCount = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            while (numbers[i] != 0) {
-                digitsCount[Math.abs((numbers[i] % 10))]++;
-                numbers[i] /= 10;
-            }
-            for (int j = 0; j < 10; j++) {
-                if (digitsCount[j] > 0) differentDigitsCount[i]++;
-            }
-            digitsCount = new int[10];
-        }
-        int min = differentDigitsCount[0];
-        for (int i = 0; i < differentDigitsCount.length; i++) {
-            if (differentDigitsCount[i] < min) {
-                min = differentDigitsCount[i];
-                minIndexinArray = i;
-            }
-        }
-        System.out.println("Число, в котором количество различных цифр минимально: " + array[minIndexinArray]);
+
     }
 }
